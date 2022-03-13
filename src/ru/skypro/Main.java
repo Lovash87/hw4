1e7f5da9f392f0b31d9b8915ab811c2cee5517a4
Vladimir Ovodov <100378594+Lovash87@users.noreply.github.com>
2022-03-13 21:05:02 +0700

    hw complete

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,34 @@
 package ru.skypro;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	     // задание 1;
+
+        int currentNum = 0;
+        while (currentNum < 10) {
+            System.out.println(++currentNum);}
+        System.out.println();
+        for (; currentNum > 0; currentNum--){
+            System.out.println(currentNum);
+        }
+
+        // задание 2;
+        int firstFridy = 2;
+        for (int currentFridy = firstFridy; currentFridy <= 31; currentFridy += 2) {
+            System.out.println("Подготовьте отчет, сегодня пятница " + currentFridy);
+        }
+
+        // задание 3;
+        int currentYear = LocalDate.now().getYear();
+        int start = currentYear - 200;
+        int finish = currentYear + 100;
+        for (int year = start; year <= finish; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
+            }
+        }
     }
 }
